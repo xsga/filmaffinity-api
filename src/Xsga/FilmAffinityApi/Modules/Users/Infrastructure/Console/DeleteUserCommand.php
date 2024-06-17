9d70c82ccc13c1977dc9e3d1e66702eb60233f84
parker <67624778+xsga@users.noreply.github.com>
2024-06-17 10:54:12 +0000

    Unused code

--- src/Xsga/FilmAffinityApi/Modules/Users/Infrastructure/Console/DeleteUserCommand.php
+++ src/Xsga/FilmAffinityApi/Modules/Users/Infrastructure/Console/DeleteUserCommand.php
@@ -11,7 +11,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Xsga\FilmAffinityApi\Modules\Users\Application\Services\DeleteUserByEmailService;
-use Xsga\FilmAffinityApi\Modules\Users\Application\Services\UpdateUserStatusService;
 use Xsga\FilmAffinityApi\Modules\Users\Domain\ValueObjects\UserEmail;
 
 #[AsCommand(
