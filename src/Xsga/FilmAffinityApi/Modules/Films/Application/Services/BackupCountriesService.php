372508f440350f2ff42251fcf2257dd28f533825
parker <67624778+xsga@users.noreply.github.com>
2024-10-02 07:25:52 +0000

    Code revision

--- src/Xsga/FilmAffinityApi/Modules/Films/Application/Services/BackupCountriesService.php
+++ src/Xsga/FilmAffinityApi/Modules/Films/Application/Services/BackupCountriesService.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 namespace Xsga\FilmAffinityApi\Modules\Films\Application\Services;
 
+use Log4Php\Logger;
 use Throwable;
 use Xsga\FilmAffinityApi\Modules\Films\Infrastructure\Repositories\FilmAffinityCountriesRepository;
 
 final class BackupCountriesService
 {
     public function __construct(
+        private Logger $logger,
         private FilmAffinityCountriesRepository $repository,
         private string $language,
         private string $destinationPath
@@ -24,7 +26,10 @@ final class BackupCountriesService
             $fileName      = "countries_$this->language.json";
 
             file_put_contents($this->destinationPath . $fileName, $countriesJson);
-        } catch (Throwable $th) {
+        } catch (Throwable $exception) {
+            $this->logger->error('Error storing countries backup');
+            $this->logger->error($exception->__toString());
+            
             return false;
         }
 
--- src/Xsga/FilmAffinityApi/Modules/Films/Application/Services/BackupGenresService.php
+++ src/Xsga/FilmAffinityApi/Modules/Films/Application/Services/BackupGenresService.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 namespace Xsga\FilmAffinityApi\Modules\Films\Application\Services;
 
+use Log4Php\Logger;
 use Throwable;
 use Xsga\FilmAffinityApi\Modules\Films\Infrastructure\Repositories\FilmAffinityGenresRepository;
 
 final class BackupGenresService
 {
     public function __construct(
+        private Logger $logger,
         private FilmAffinityGenresRepository $repository,
         private string $language,
         private string $destinationPath
@@ -24,7 +26,10 @@ final class BackupGenresService
             $fileName   = "genres_$this->language.json";
 
             file_put_contents($this->destinationPath . $fileName, $genresJson);
-        } catch (Throwable $th) {
+        } catch (Throwable $exception) {
+            $this->logger->error('Error storing genres backup');
+            $this->logger->error($exception->__toString());
+            
             return false;
         }
 
